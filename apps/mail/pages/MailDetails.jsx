0e16bf93fd1d2c07ad90b7e013e64f74f28e66c3
Digidigidoom <tomshahar91@gmail.com>
2025-04-17 13:45:46 +0300

    Add mail.service

--- apps/mail/pages/MailDetails.jsx
+++ apps/mail/pages/MailDetails.jsx
@@ -1,10 +1,15 @@
-const { useState, useEffect } = React;
-import { useNavigate } from "react-router-dom"; // For routing/navigation
+const { useParams, useNavigate, Link } = ReactRouterDOM
+const { useState, useEffect } = React
 
 
 
 export function MailDetails() {
+    const navigate = useNavigate()
 
+
+    function onBack(){
+            navigate('/mail')
+    }
     return (
         <React.Fragment>
             <div className="mail-details-container">
@@ -13,6 +18,7 @@ export function MailDetails() {
                 <p className="mail-subject">Sign up Now</p>
                 <p className="mail-body">hey hey hey hey hey hey hey hey hey hey hey hey hey hey hey </p>
                 <p className="mail-from">momo@momo.com</p>
+                <button className="btn back-btn-details" onClick={onBack}>Back</button>
             </div>
         </React.Fragment>
     )
