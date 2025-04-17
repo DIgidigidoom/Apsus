befbad5d1249a06c26efce2aa5a50850cdee474b
hadar <hadarsabag5@gmail.com>
2025-04-17 13:27:59 +0300

    Add note

--- apps/note/pages/NoteIndex.jsx
+++ apps/note/pages/NoteIndex.jsx
@@ -1,4 +1,24 @@
+import { noteService } from "../services/note.service.js"
+
+const { useState, useEffect } = React
 
 export function NoteIndex() {
+
+    const [notes, setNotes] = useState(null)
+
+    useEffect(() => {
+        loadNotes()
+    }, [])
+
+    function loadNotes() {
+        noteService.query()
+            .then(notes => setNotes(notes))
+            .catch(err => {
+                console.log('err:', err)
+                showErrorMsg('Cannot get notes!')
+            })
+    }
+
+console.log("notes: ", notes)
     return <div>note app</div>
 }
