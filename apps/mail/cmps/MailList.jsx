88bf79659b033b518a24d2c13dd40356b18843dc
Digidigidoom <tomshahar91@gmail.com>
2025-04-18 11:16:35 +0300

    Add toggle isRead

--- apps/mail/cmps/MailList.jsx
+++ apps/mail/cmps/MailList.jsx
@@ -4,7 +4,7 @@ const { Link } = ReactRouterDOM
 const { useState, useEffect } = React
 
 export function MailList({ mails, onRemoveMail }) {
-    
+
 
     function onSetIsRead(id) {
         mailService.get(id)
@@ -15,20 +15,35 @@ export function MailList({ mails, onRemoveMail }) {
 
     }
 
+    function onToggleIsRead(id, ev) {
+        ev.stopPropagation()
+        ev.preventDefault()
+        mailService.get(id)
+            .then(mail => {
+                mail.isRead = !mail.isRead
+                mailService.save(mail)
+            })
+    }
+
     if (!mails.length) return <div>No Mails To Show...</div>
     return (
         <React.Fragment>
             <ul className="mail-list-container">
                 {mails.map(mail => (
                     <Link key={mail.id} mail={mail} to={`/mail/${mail.id}`}>
-                        <li key={mail.id} className={!mail.isRead ? "not-read" : ""} onClick={() => onSetIsRead(mail.id)} >
+                        <li className={!mail.isRead ? "not-read" : ""} onClick={() => onSetIsRead(mail.id)} >
                             <section className="mail-left-buttons">
                                 <button className="fa-regular fa-square"></button>
                                 <button className="fa-regular fa-star"></button>
                             </section>
                             <MailPreview mail={mail} />
                             <section className="mail-right-btns">
-                                <button onClick={(event) => onRemoveMail(mail.id,event)} className="btn remove-mail-list-btn fa-solid fa-trash"></button>
+                                <button onClick={(event) => onRemoveMail(mail.id, event)} className="btn remove-mail-list-btn fa-solid fa-trash"></button>
+                                <button
+                                    className={`btn toggle-read-list-btn fa-solid 
+                                    ${!mail.isRead ? 'fa-envelope-open' : 'fa-envelope'}`}
+                                    onClick={(e) => {onToggleIsRead(mail.id,e)}}>
+                                </button>
                             </section>
 
                         </li>
--- apps/mail/cmps/MailPreview.jsx
+++ apps/mail/cmps/MailPreview.jsx
@@ -1,11 +1,24 @@
-export function MailPreview({mail}) {
+export function MailPreview({ mail }) {
+
+
+    function onFormatDate() {
+        const sent = new Date(mail.sentAt)
+        const day = String(sent.getDate()).padStart(2, '0');
+        const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+            "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"]
+        const month = monthNames[sent.getMonth()]
+        const formattedDate = `${month} ${day}`
+        return formattedDate
+    }
+
+
     return (
-        <div className="mail-preview-container"> 
+        <div className="mail-preview-container">
             <p className="mail-from">{mail.from}</p>
             <p className="mail-subject">{mail.subject}</p>
             <p className="mail-body">- {mail.body}</p>
-            <p className="mail-recieve-time"> {mail.sentAt}</p>
-            
+            <p className="mail-recieve-time"> {onFormatDate()}</p>
+
         </div>
     )
 }
