bdc0696b01e2c7b28140ada5f8ca1b1e6604fb71
hadar <hadarsabag5@gmail.com>
2025-04-18 17:30:09 +0300

    add icon

--- apps/note/cmps/NoteList.jsx
+++ apps/note/cmps/NoteList.jsx
@@ -2,14 +2,14 @@ import { NotePreview } from "./NotePreview.jsx";
 
 const { Link } = ReactRouterDOM
 
-export function NoteList({ notes,  onRemoveNote }) {
+export function NoteList({ notes, onRemoveNote }) {
     return (
         <div className="note-list-container flex">
             {notes.map(note => (
                 <section className="note-preview" key={note.id}>
                     <NotePreview note={note} />
                     <button className="btn remove-note-btn" onClick={() => onRemoveNote(note.id)}>
-                        Remove
+                        <i className="fa-solid fa-trash"></i>
                     </button>
                     <button className="btn note-details-btn" >
                         <Link to={`/note/details/`}>Details</Link>
