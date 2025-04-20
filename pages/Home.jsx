cbf6053b82a7ebfbe36890d0a9d8e7debe9ac19e
Digidigidoom <tomshahar91@gmail.com>
2025-04-20 20:44:50 +0300

    Maybe?

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -1,21 +1,19 @@
 const { Link, NavLink } = ReactRouterDOM
-import gmailLogo from '../assets/img/Gmail-logo-home.png'
-
 
 
 export function Home() {
 
     return <section className="home">
         <div className="main-logo-container">
-            <img src="./assets/img/Apsus-logo.png" alt="" />
+            <img src="assets/img/Apsus-logo.png" alt="" />
             <p>All Your Friendly Apps In One Place</p>
         </div>
         <div className="icons-nav-container">
             <NavLink to="/mail">
-                <img className="mail-logo-home" src="./assets/img/Gmail-logo-home.png" alt="" />
+                <img className="mail-logo-home" src="assets/img/Gmail-logo-home.png" alt="" />
             </NavLink>
             <NavLink to="/note">
-                <img className="note-logo-home" src="./assets/img/note-logo.png" alt="" />
+                <img className="note-logo-home" src="assets/img/note-logo.png" alt="" />
             </NavLink>
         </div>
     </section>
