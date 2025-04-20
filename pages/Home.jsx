f5ce0febee4c16f9f2931d706c5d4100581a73dc
Digidigidoom <tomshahar91@gmail.com>
2025-04-20 20:33:09 +0300

    never give up?

--- pages/Home.jsx
+++ pages/Home.jsx
@@ -12,10 +12,10 @@ export function Home() {
         </div>
         <div className="icons-nav-container">
             <NavLink to="/mail">
-                <img className="mail-logo-home" src={gmailLogo} alt="" />
+                <img className="mail-logo-home" src="assets/img/Gmail-logo-home.png" alt="" />
             </NavLink>
             <NavLink to="/note">
-                <img className="note-logo-home" src=".../assets/img/note-logo.png" alt="" />
+                <img className="note-logo-home" src="assets/img/note-logo.png" alt="" />
             </NavLink>
         </div>
     </section>
